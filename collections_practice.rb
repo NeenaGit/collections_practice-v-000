77a3e7627c30a41f81c7b8e81e70af3e8017f452
Neena van de Water <nkuriakose@hotmail.com>
2017-11-16 19:55:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
 
+def sort_array_asc (array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort { | x,y | y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort_by { | x | x.length}
+end
+
+def swap_elements(array)
+  array.insert(1, array.delete_at(2))
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  money_signed = [ ]
+  array.each_with_index do | word, index|
+     money_signed << word = "$", index = 2
+  end
+  money_signed
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject(0) { | sum, x | sum + x }
+end
+
+def add_s(array)
+array.each_with_index.collect { |s , i| s.prepend(' s '), index-1}
+end
